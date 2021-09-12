f8a8b5044cde58812fe45207baab7e8977a2777f
Salkoazer <wilsonveterano@gmail.com>
2021-09-12 15:28:50 +0100

    .

---
+++ src/main/java/com/example/krakenapp/KrakenappApplication.java
@@ -0,0 +1,13 @@
+package com.example.krakenapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KrakenappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KrakenappApplication.class, args);
+	}
+
+}
